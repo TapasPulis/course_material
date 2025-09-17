c3d77cbe6590a52330070050ff975b48dd7e88ef
TapasPulis <rooneysamragya10@gmail.com>
2025-09-17 12:14:59 +0200

    Went through JavaScript math operators

---
+++ JS/03-JS_math_operators/script.js
@@ -0,0 +1,19 @@
+let A = 2;
+let B = 6;
+let C = 10;
+
+const result = A + B + C;
+console.log(A - B - C);
+console.log(A * B * C);
+console.log(A / B / C);
+
+console.log(result > 50 && !result > 100);
+
+const score = 85;
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else {
+  console.log("C");
+}
