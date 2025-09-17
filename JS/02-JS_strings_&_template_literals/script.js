497c5970ca41ab9b69c924343b0d43929d83ca9f
TapasPulis <rooneysamragya10@gmail.com>
2025-09-17 12:13:55 +0200

    Went through JavaScript strings and template literals

---
+++ JS/02-JS_strings_&_template_literals/script.js
@@ -0,0 +1,14 @@
+const first_name = "John";
+const last_name = "Doe";
+const quote = "Life is like a box of chocolates.";
+const phrase = "'You're gonna need a bigger boat.'";
+
+let day = "16";
+let month = "September";
+let year = "2025";
+
+console.log(`Today, it's the ${day}th of ${month} in the year ${year}.`);
+
+console.log(`Hello my name is ${first_name} ${last_name}.`);
+console.log(quote.length);
+console.log(phrase);
