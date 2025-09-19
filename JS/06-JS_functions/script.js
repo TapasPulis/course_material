633a8f68dfccf9e2b6b948880855b68ae750b318
TapasPulis <rooneysamragya10@gmail.com>
2025-09-19 11:07:14 +0200

    Updated JavaScript functions

--- JS/06-JS_functions/script.js
+++ JS/06-JS_functions/script.js
@@ -14,3 +14,44 @@ const calcBMI = function (name, height, weight) {
 };
 
 console.log(calcBMI("Brudda", 1.65, 58));
+
+// arrow function
+// * First way: without curly brackets
+const subtract = (a, b) => a - b;
+console.log(subtract(2, 3));
+
+// * Second way: with curly brackets
+const subtract2 = (a, b) => {
+  return a - b;
+};
+console.log(subtract2(2, 3));
+
+// arrow function with default parameter
+const calculateAge = (birthYear, currentYear = 2025) => {
+  return currentYear - birthYear;
+};
+
+console.log(calculateAge(1999));
+
+// grade uppgift
+const calculateGrade = (name, score) => {
+  if (score >= 90 && score <= 100) return `${name}'s grade is A.`;
+  if (score >= 80 && score <= 89) return `${name}'s grade is B.`;
+  if (score >= 70 && score <= 79) return `${name}'s grade is C.`;
+  if (score >= 60 && score <= 69) return `${name}'s grade is D.`;
+  if (score < 60 && score >= 0) return `${name}'s grade is F.`;
+  if (score > 100 || score < 0) return `Invalid score for ${name}.`;
+};
+
+console.log(calculateGrade("Alice", -1));
+
+/* function calculateGrad(name, score) {
+  if (score >= 90 && score <= 100) return `${name}'s grade is A.`;
+  if (score >= 80 && score <= 89) return `${name}'s grade is B.`;
+  if (score >= 70 && score <= 79) return `${name}'s grade is C.`;
+  if (score >= 60 && score <= 69) return `${name}'s grade is D.`;
+  if (score < 60 && score >= 0) return `${name}'s grade is F.`;
+  if (score > 100 || score < 0) return `Invalid score for ${name}.`;
+}
+
+console.log(calculateGrad("Alice", 96)); */
